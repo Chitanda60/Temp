871c41b7b6bc5f15ca80bb3d32ebba2dc4b42dd3
Chitanda60 <freeyinyutian@gmail.com>
2017-03-21 15:51:14 +0800

    日常提交

--- es6test.js
+++ es6test.js
@@ -7,3 +7,63 @@ test(60)
 
 let x = 1
 console.log(`${x}fish`)
+
+// const shemei = (x) => (y) => {
+// 	return x(y)
+// }
+
+const shemei = function(x) {
+	return function(y) {
+		return x(y)
+	}
+}
+
+const zhixia = shemei(function(v) {
+	console.log(v)
+})
+
+zhixia('你好');
+
+const funA = (func) => {	
+	return () => {
+		console.log('funA')
+		func()
+	}
+}
+const funB = (func) => {
+	return () => {
+		console.log('funB')
+		func()
+	}
+}
+const funC = (func) => {
+	return () => {
+		console.log('funC')
+		func()
+	}
+}
+
+const arr = [funA, funB, funC]
+const funcAll = arr.reduceRight((composed, f) => f(composed), () => {
+	console.log('initial func')
+})
+funcAll()
+
+async function foo() {
+	const a = await new Promise(function(resolve, reject) {
+		window.setTimeout(function() {
+			resolve({a: 12})
+		}, 2000)
+	})
+	console.log(a.a)
+	return a.a
+}
+foo()
+
+
+
+
+
+
+
+
