4e2c7cef074abcb14874ce8f377bd171cdb0c1e5
Chitanda60 <freeyinyutian@gmail.com>
2017-04-25 15:40:47 +0800

    日常提交

---
+++ autodeploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+WEB_PATH='/usr/share/nginx/html/fed'
+WEB_USER='root'
+WEB_USERGROUP='lovelucydev'
+
+echo "Start deployment"
+cd $WEB_PATH
+echo "pulling source code..."
+git reset --hard origin/master
+git clean -f
+git pull
+git checkout master
+# echo "changing permissions..."
+# chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+echo "Finished."
